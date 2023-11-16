656343cfec16931b9b90960fb7aea766fb92e30e
Farhan Sadeek <farhansadeek019@gmail.com>
2023-11-15 23:33:56 -0500

    update

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,13 @@
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+import tensorflow as tf
+
 
 # Load your dataset and preprocess 'Label' as you mentioned before
 df = pd.read_csv('data.csv')
@@ -15,18 +21,61 @@ y = df['Label'].values
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Vectorize the text data (you can use TF-IDF or other methods)
-from sklearn.feature_extraction.text import TfidfVectorizer
+# Vectorize the text data using TF-IDF
 vectorizer = TfidfVectorizer()
-X_train = vectorizer.fit_transform(X_train)
-X_test = vectorizer.transform(X_test)
+X_train = vectorizer.fit_transform(X_train).toarray()
+X_test = vectorizer.transform(X_test).toarray()
 
-# Create and fit the Logistic Regression model
-logreg_classifier = LogisticRegression(random_state=42)
-logreg_classifier.fit(X_train, y_train)
+# Convert NumPy arrays to PyTorch tensors
+X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
+X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+y_test_tensor = torch.tensor(y_test, dtype=torch.float32)
+
+# Create a neural network model using PyTorch
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x
+
+# Define model, loss function, and optimizer
+input_size = X_train.shape[1]
+hidden_size = 64
+output_size = 1
+
+model = NeuralNetwork(input_size, hidden_size, output_size)
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Train the model
+epochs = 5
+for epoch in range(epochs):
+    # Forward pass
+    outputs = model(X_train_tensor)
+    loss = criterion(outputs, y_train_tensor.view(-1, 1))
+
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
 # Make predictions on the test set
-y_pred = logreg_classifier.predict(X_test)
+with torch.no_grad():
+    y_pred_probs = model(X_test_tensor)
+    y_pred = (y_pred_probs > 0.5).float()
+
+# Convert predictions to NumPy arrays
+y_pred = y_pred.numpy()
 
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
