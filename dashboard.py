d57e65714e439b525d4e777f7f3093b016ecc633
Farhan Sadeek <farhansadeek019@gmail.com>
2023-11-15 23:58:32 -0500

    dashboard and analysis

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,7 @@
+import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import TfidfVectorizer
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import tensorflow as tf
-
+from sklearn.linear_model import LogisticRegression
 
 # Load your dataset and preprocess 'Label' as you mentioned before
 df = pd.read_csv('data.csv')
@@ -18,65 +12,36 @@ df = df.drop('ID', axis=1)
 X = df['Text'].values
 y = df['Label'].values
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Vectorize the text data using TF-IDF
+# Vectorize the text data using TF-IDF from scikit-learn
 vectorizer = TfidfVectorizer()
-X_train = vectorizer.fit_transform(X_train).toarray()
-X_test = vectorizer.transform(X_test).toarray()
+X_tfidf = vectorizer.fit_transform(X)
 
-# Convert NumPy arrays to PyTorch tensors
-X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
-y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
-X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
-y_test_tensor = torch.tensor(y_test, dtype=torch.float32)
+# Create and fit the Logistic Regression model
+logreg_classifier = LogisticRegression(random_state=17)
+logreg_classifier.fit(X_tfidf, y)
 
-# Create a neural network model using PyTorch
-class NeuralNetwork(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
-        self.sigmoid = nn.Sigmoid()
+# Function to predict if a text is real or fake
+def predict_text(label_text):
+    # Vectorize the input text using the same vectorizer
+    text_tfidf = vectorizer.transform([label_text])
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.sigmoid(x)
-        return x
+    # Make a prediction
+    prediction = logreg_classifier.predict(text_tfidf)
 
-# Define model, loss function, and optimizer
-input_size = X_train.shape[1]
-hidden_size = 64
-output_size = 1
+    # Return the prediction
+    return "REAL" if prediction[0] == 1 else "FAKE"
 
-model = NeuralNetwork(input_size, hidden_size, output_size)
-criterion = nn.BCELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+# Streamlit app
+def main():
+    st.title("Fake News Detection")
 
-# Train the model
-epochs = 5
-for epoch in range(epochs):
-    # Forward pass
-    outputs = model(X_train_tensor)
-    loss = criterion(outputs, y_train_tensor.view(-1, 1))
+    # User input text
+    user_input = st.text_area("Enter a text:", "This is a sample text.")
 
-    # Backward pass and optimization
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    # Make prediction on button click
+    if st.button("Predict"):
+        result = predict_text(user_input)
+        st.success(f"The model predicts: {result}")
 
-# Make predictions on the test set
-with torch.no_grad():
-    y_pred_probs = model(X_test_tensor)
-    y_pred = (y_pred_probs > 0.5).float()
-
-# Convert predictions to NumPy arrays
-y_pred = y_pred.numpy()
-
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy * 100:.2f}%")
+if __name__ == "__main__":
+    main()
